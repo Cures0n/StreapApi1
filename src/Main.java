14b3ee95e65a923468ca50a3d583e4c019aa4ac7
Ivan <Ivan4ik.ru70@gmail.com>
2022-04-13 21:55:18 +0700

    done filtering

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < intList.size(); i++) {
+            if (0 < intList.get(i) && intList.get(i) % 2 == 0 ) {
+                list.add(intList.get(i));
+            }
+        }
+
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
